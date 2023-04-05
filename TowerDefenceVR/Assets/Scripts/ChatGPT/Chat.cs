5f388fb78149c522b55d699704dcffd74cf4ff74
M.R <m.r.MeijoUniv@gmail.com>
2023-04-06 00:25:15 +0900

    [add]プレイヤー周りのUI追加

---
+++ TowerDefenceVR/Assets/Scripts/ChatGPT/Chat.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Chat : MonoBehaviour
+{
+    //[SerializeField] ChatMessageView messageViewTemplete;
+    [SerializeField] private TMP_Text testText;
+    [SerializeField] TMP_InputField inputField; //魔法の入力フィールド
+    //[SerializeField] ScrollRect scrollRect;
+    [SerializeField] Button generateButton; //生成ボタン
+
+    [SerializeField] string apiKey; // NOTE: 入力したままコミットやリポジトリの公開などをしないこと
+
+    OpenAIChatCompletionAPI chatCompletionAPI;
+
+    //初期メッセージを定義
+    List<OpenAIChatCompletionAPI.Message> context = new List<OpenAIChatCompletionAPI.Message>()
+    {
+        new OpenAIChatCompletionAPI.Message(){
+            role = "system", 
+            content = @"あなたは魔法の創造者です。私が挙げる魔法名に対して以下の情報を推察し、次に指定するフォーマットで回答してください。
+                        {
+                        回答のフォーマット:
+                        属性:<魔法名から推察された属性>
+                        威力:<魔法名から推察された威力の値(1-100)>
+                        }
+                        <魔法名から推察された属性>は<火><氷><水><雷><風><土><光><闇>のいずれかとします。推察できない魔法であった場合は<無>としてください。
+                        <魔法名から推察された威力>は最も弱いものを1、最も強いものを100として整数値で出来るだけ弱く推察してください。
+
+                        回答のフォーマットに即した回答のみ答えてください。
+                        次の会話からスタートします"
+        }
+    };
+
+    void Awake()
+    {
+        //messageViewTemplete.gameObject.SetActive(false);
+        //generateButton.onClick.AddListener(OnSendClick);
+        chatCompletionAPI = new OpenAIChatCompletionAPI(apiKey);
+    }
+
+    public void OnGenerateButtonClick()
+    {
+        //何も入力されていない、もしくはNullであれば終了
+        if (string.IsNullOrEmpty(inputField.text)) return;
+        //新たな魔法の入力を受け付ける
+        var message = new OpenAIChatCompletionAPI.Message() { role = "user", content = inputField.text };
+        context.Add(message);
+        //AppendMessage(message);
+        inputField.text = "";
+        //ChatGPTとの通信準備開始
+        StartCoroutine(ChatCompletionRequest());
+    }
+
+    IEnumerator ChatCompletionRequest()
+    {
+        //回答が帰ってくるまでは新たな生成は受け付けない
+        generateButton.interactable = false;
+
+        //1フレーム中断して再開する(非同期処理)
+        yield return null;
+        //scrollRect.verticalNormalizedPosition = 0;
+        //入力内容から新たなリクエストを生成する
+        var request = chatCompletionAPI.CreateCompletionRequest(
+            new OpenAIChatCompletionAPI.RequestData() { messages = context }
+        );
+        //リクエストを送信する
+        yield return request.Send();
+        //レスポンスがエラーであった場合はエラー処理
+        if (request.IsError) throw new System.Exception(request.Error);
+        //レスポンスのリストの中から最も新しいレスポンス内容を取得する
+        var message = request.Response.choices[0].message;
+
+        testText.text = message.content;
+        //AppendMessage(message);
+        //1フレーム中断して再開(非同期処理)
+        yield return null;
+        //scrollRect.verticalNormalizedPosition = 0;
+        //新たな魔法の生成を受け付けられるようにする
+        generateButton.interactable = true;
+    }
+
+    void AppendMessage(OpenAIChatCompletionAPI.Message message)
+    {
+        context.Add(message);
+        /*
+        var messageView = Instantiate(messageViewTemplete);
+        messageView.gameObject.name = "message";
+        messageView.gameObject.SetActive(true);
+        messageView.transform.SetParent(messageViewTemplete.transform.parent, false);
+        messageView.Role = message.role;
+        messageView.Content = message.content;
+
+        if(message.role != "user")
+        {
+            //saveScript.ExtractCode(message.content);
+        }
+        */
+    }
+}
---
+++ TowerDefenceVR/Assets/Scripts/PlayerUIManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    public TMP_Text generateText;
+    [SerializeField] private Chat chat;
+    [SerializeField] private GameObject playerMagicsCanvas;
+    [SerializeField] private GameObject playerNewMagicCanvas;
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private TMP_InputField magicInputField;
+    private bool canvasEnable = false;
+    private TouchScreenKeyboard keyboard;
+
+    private void Awake() 
+    {
+        playerMagicsCanvas.SetActive(canvasEnable);
+        playerNewMagicCanvas.SetActive(canvasEnable);
+    }
+
+    private void Update() 
+    {
+        //Xボタンを押したら魔法生成UIを表示・非表示を切り替える
+        if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch))
+        {
+            canvasEnable = !canvasEnable;
+            playerMagicsCanvas.SetActive(canvasEnable);
+            if(mainCamera != null)
+            {
+                playerMagicsCanvas.transform.position = mainCamera.transform.position + mainCamera.transform.forward * 1.5f;
+                playerNewMagicCanvas.transform.position = mainCamera.transform.position + mainCamera.transform.forward * 1.5f - mainCamera.transform.up * 1.0f;
+
+                Vector3 lookPos1 = mainCamera.transform.position - playerMagicsCanvas.transform.position;
+                Vector3 lookPos2 = mainCamera.transform.position - playerNewMagicCanvas.transform.position;
+
+                playerMagicsCanvas.transform.rotation = Quaternion.LookRotation(lookPos1);
+                playerMagicsCanvas.transform.Rotate(Vector3.up, 180f, Space.Self);
+                playerNewMagicCanvas.transform.rotation = Quaternion.LookRotation(lookPos2);
+                playerNewMagicCanvas.transform.Rotate(Vector3.up, 180f, Space.Self);
+            }
+        }
+
+        if(keyboard != null)
+        {
+            magicInputField.text = keyboard.text;
+        }
+    }
+
+    public void OnNewMagicButton()
+    {
+        playerMagicsCanvas.SetActive(false);
+        playerNewMagicCanvas.SetActive(true);
+
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+    }
+
+    public void OnGenerateButton()
+    {
+        chat.OnGenerateButtonClick();
+    }
+
+    public void OnBackButton()
+    {
+        playerNewMagicCanvas.SetActive(false);
+        playerMagicsCanvas.SetActive(true);
+    }
+
+    public void OnEndButton()
+    {
+        playerMagicsCanvas.SetActive(false);
+        playerNewMagicCanvas.SetActive(false);
+        canvasEnable = false;
+    }
+}
